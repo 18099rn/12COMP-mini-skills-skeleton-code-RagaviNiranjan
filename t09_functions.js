ae90e67687e5aaaa33d3668ec73dbab04aee43e9
Ragavi Niranjan <18099rn@hvhs.school.nz>
2021-03-04 23:30:00 +0000

    Changed page

--- t09_functions.js
+++ t09_functions.js
@@ -1,6 +1,7 @@
 /********************************************************/
 //
 /********************************************************/
+
 var ball1 = {
   diameter: 10,
   posX: 0,
@@ -9,119 +10,59 @@ var ball1 = {
   velY: 0,
   R: 0,
   G: 0,
-  B: 0
-}
+  B: 0,
 
-var ball2 = {
-  diameter: 10,
-  posX: 0,
-  posY: 0,
-  velX: 0,
-  velY: 0,
-  R: 0,
-  G: 0,
-  B: 0
-}
+  init: function (){
+    this.posX = random(this.diameter/2, width - this.diameter/2);
+    this.posY = random(this.diameter/2, width - this.diameter/2);
+    this.velX = random(5, 10);
+    this.velY = random(4, 12); 
+  },
 
+  move: function(){
+    this.posX = this.posX + this.velX;
+    this.posY= this.posY + this.velY;
+  },
 
-  function moveBall (){
-
-  this.posX = this.posX + this.velX;
-  this.posY= this.posY + this.velY;
-  
+  bounce: function(){
+    if ( this.posX >= width - this.diameter / 2) {
+      fill(random(0, 255), random(0, 255), random(0, 255));
+      this.velX = this.velX * -1;
+      this.posX = width - this.diameter / 2;
   }
-
-  function bounceBall (){
-
-     function bounceBall1() {
-
-
- if ( this.posX >= width - this.diameter / 2) {
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    this.velX = this.velX * -1;
-     this.posX = width - this.diameter / 2;
+    if ( this.posX <= this.diameter / 2) {
+      fill(random(0, 255), random(0, 255), random(0, 255));
+      this.velX = this.velX * -1;
+      this.posX = this.diameter / 2;
   }
-  if ( this.posX<= this.diameter / 2) {
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    this.velX = this.velX * -1;
-     this.posX = this.diameter / 2;
-
-  }
-
   if (this.posY >= height - this.diameter / 2) {
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    this.velY = this.velY * -1;
-    this.posY = height - this.diameter / 2;
-
+      fill(random(0, 255), random(0, 255), random(0, 255));
+      this.velY = this.velY * -1;
+      this.posY = height - this.diameter / 2;
   }
-  if (ball1.posY <= ball1.diameter / 2) {
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    ball1.velY = ball1.velY * -1;
-    ball1.posY = ball1.diameter / 2;
-    
-  }
+  if (this.posY <= this.diameter / 2) {
+      fill(random(0, 255), random(0, 255), random(0, 255));
+      this.velY = this.velY * -1;
+      this.posY = this.diameter / 2;
+    }
+  },
 
-  }
-
+  disp: function() {
+    ellipse(this.posX, this.posY, this.diameter);
+  },
+}
 
 function setup() {
   createCanvas(400, 400);
-  ball1.posX = random(ball1.diameter/2, width - ball1.diameter/2);
-  ball1.posY = random(ball1.diameter/2, width - ball1.diameter/2);
-  ball1.velX = random(5, 10);
-  ball1.velY = random(4, 12);
-
-  ball2.posX = random(ball2.diameter/2, width - ball2.diameter/2);
-  ball2.posY = random(ball2.diameter/2, width - ball2.diameter/2);
-  ball2.velX = random(3, 10);
-  ball2.velY = random(5, 19);
+  ball1.init();
 }
-
 function draw() {
-   background('black');
-   move
-
- 
-
- ball2.posX = ball2.posX + ball2.velX;
-ball2.posY = ball2.posY + ball2.velY;
-
-}
-
-  ellipse(ball1.posX, ball1.posY, ball1.diameter);
-
-  function bounceBall2(){
-
-     if (ball2.posX >= width - ball2.diameter / 2) {
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    ball2.velX = ball2.velX * -1;
-    ball2.posX = width - ball2.diameter / 2;
-
-  }
-  if (ball2.posX <= ball2.diameter / 2) {
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    ball2.velX = ball2.velX * -1;
-   ball2.posX = ball2.diameter / 2;
-
+  background('black');
+  ball1.move();
+  ball1.bounce();
+  ball1.disp();
   }
 
-  if (ball2.posY >= height - ball2.diameter / 2) {
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    ball2.velY = ball2.velY * -1;
-    ball2.posY = height - ball2.diameter / 2;
+    
 
-  }
-  if (ball2.posY <= ball2.diameter / 2) {
-    fill(random(0, 255), random(0, 255), random(0, 255));
-   ball2.velY = ball2.velY * -1;
-    ball2.posY = ball2.diameter / 2;
-
-
-  }
-
-  
-  }
-
-  ellipse(ball2.posX, ball2.posY, ball2.diameter);
-}
 
--- t10_array_of_objects.js
+++ t10_array_of_objects.js
@@ -1,10 +1,86 @@
 /********************************************************/
 //
 /********************************************************/
+
+
 function setup() {
   createCanvas(400, 400);
+  const numBalls = 3;
+  var ballArray = [];
+  
+  
+var ball1 = {
+  diameter: 10,
+  posX: 0,
+  posY: 0,
+  velX: 0,
+  velY: 0,
+  R: 0,
+  G: 0,
+  B: 0,
+
+  init: function (){
+    this.posX = random(this.diameter/2, width - this.diameter/2);
+    this.posY = random(this.diameter/2, width - this.diameter/2);
+    this.velX = random(5, 10);
+    this.velY = random(4, 12); 
+  },
+
+  move: function(){
+    this.posX = this.posX + this.velX;
+    this.posY= this.posY + this.velY;
+  },
+
+  bounce: function(){
+    if ( this.posX >= width - this.diameter / 2) {
+      fill(random(0, 255), random(0, 255), random(0, 255));
+      this.velX = this.velX * -1;
+      this.posX = width - this.diameter / 2;
+  }
+    if ( this.posX <= this.diameter / 2) {
+      fill(random(0, 255), random(0, 255), random(0, 255));
+      this.velX = this.velX * -1;
+      this.posX = this.diameter / 2;
+  }
+  if (this.posY >= height - this.diameter / 2) {
+      fill(random(0, 255), random(0, 255), random(0, 255));
+      this.velY = this.velY * -1;
+      this.posY = height - this.diameter / 2;
+  }
+  if (this.posY <= this.diameter / 2) {
+      fill(random(0, 255), random(0, 255), random(0, 255));
+      this.velY = this.velY * -1;
+      this.posY = this.diameter / 2;
+    }
+  },
+
+  disp: function() {
+    ellipse(this.posX, this.posY, this.diameter);
+  },
+}
 }
 
+
+
 function draw() {
-  background(220);
-}
+  background('black');
+  for (var i = 0; i <numBalls.length; i++) {
+    if (numBalls<3) {
+     ballArray[i].push();
+    }
+  
+  ball1[0].move();
+  ball1[0].bounce();
+  ball1[0].disp();
+  ball1[0].init();
+  }
+  }
+
+
+
+
+
+
+  
+  
+ 
